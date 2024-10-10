9e32e047fe44bda78a397536d5a6b0b3d9a81e10
Clinton-okumu <clintonomondiokumu@gmail.com>
2024-10-10 22:50:40 +0300

    lockbox

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,26 @@
+def can_open_all_boxes(boxes):
+    """
+    Determines if all the boxes can be opened.
+
+    Args:
+        boxes: A list of lists representing the boxes and their keys.
+
+    Returns:
+        True if all boxes can be opened,    else False.
+    """
+    n = len(boxes)
+    opened = set()  # Set to keep track of opened boxes
+    keys = set([0])  # Start with key to box 0
+
+    def dfs(box):
+        if box in opened:
+            return
+        opened.add(box)
+        for key in boxes[box]:
+            keys.add(key)
+        for key in keys.copy():  # Use copy to avoid modifying set during iteration
+            if key < n and key not in opened:
+                dfs(key)
+
+    dfs(0)  # Start DFS from box 0
+    return len(opened) == n
